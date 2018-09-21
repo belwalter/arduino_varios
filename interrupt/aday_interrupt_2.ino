5df64812cba4651818e73e991cd210c7cebe454a
belwalter <belwalterv@gmail.com>
2018-09-21 20:32:17 -0300

    interrupciones

---
+++ interrupt/aday_interrupt_2.ino
@@ -0,0 +1,35 @@
+#define pin_int 2
+#define pin_sig 9
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pin_sig, OUTPUT);
+  digitalWrite(pin_sig, LOW);
+  pinMode(LED_BUILTIN, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(pin_int), subida, RISING);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(pin_sig, HIGH);
+  delay(5000);
+  digitalWrite(pin_sig, LOW);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(3000);
+}
+
+void subida(){
+  Serial.println("Interrupcion de subida activada");
+  digitalWrite(LED_BUILTIN, HIGH);
+  detachInterrupt(0);
+  attachInterrupt(digitalPinToInterrupt(pin_int), bajada, FALLING);
+  }
+
+void bajada(){
+  Serial.println("Interrupcion de bajada activada");
+  digitalWrite(LED_BUILTIN, LOW);
+  detachInterrupt(0);
+  attachInterrupt(digitalPinToInterrupt(pin_int), subida, RISING);
+  }
---
+++ interrupt/aday_interrupt_3.ino
@@ -0,0 +1,31 @@
+#define pin_int 2
+#define pin_sig 9
+
+volatile int pulsos;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pin_sig, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(pin_int), contador_de_pulsos, CHANGE);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  tiempo();
+  Serial.println("retorno al loop principal");
+  delay(4000);
+}
+
+void contador_de_pulsos(){
+  pulsos++;
+}
+
+void tiempo(){
+  pulsos = 0;
+  Serial.println("inicio interrupción");
+  interrupts();
+  delay(10000);
+  noInterrupts();
+  Serial.println(pulsos);
+}
+
