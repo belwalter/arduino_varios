652f59f8bc32b328068973ef1ca7e9afc97d289e
belwalter <belwalterv@gmail.com>
2019-04-30 18:04:19 -0300

    manejo de rele y lampara

---
+++ rele/rele_lampara/rele_lampara.ino
@@ -0,0 +1,22 @@
+#define rele 8
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(rele, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0){
+    String msj = Serial.readStringUntil('.');
+    if(msj == "P" ){
+      digitalWrite(rele, HIGH);
+      Serial.println("Encender luz");
+      }
+    else if(msj == "A" ){
+      digitalWrite(rele, LOW);
+      Serial.println("Apagar luz");
+      }
+    }
+}
